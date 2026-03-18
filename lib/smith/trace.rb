2411a9e50423ad4cbf899c400b83f513add149d9
Samuel Ralak <thesamuelralak@gmail.com>
2026-03-19 01:44:24 +0300

    Support class-configured trace adapters with memoized instantiation

--- lib/smith/trace.rb
+++ lib/smith/trace.rb
@@ -3,12 +3,28 @@
 module Smith
   module Trace
     def self.record(type:, data:)
-      adapter = Smith.config.trace_adapter
+      adapter = resolve_adapter
       return unless adapter
 
       adapter.record(type: type, data: data)
     rescue StandardError => e
       Smith.config.logger&.error("Smith::Trace adapter error: #{e.message}")
+    end
+
+    def self.resolve_adapter
+      configured = Smith.config.trace_adapter
+      return nil unless configured
+
+      if configured.is_a?(Class)
+        @adapter_instances ||= {}
+        @adapter_instances[configured] ||= configured.new
+      else
+        configured
+      end
+    end
+
+    def self.reset!
+      @adapter_instances = nil
     end
   end
 end
